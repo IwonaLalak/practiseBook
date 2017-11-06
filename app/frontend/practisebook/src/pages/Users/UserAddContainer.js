afd1b5bac1672fa4faa80f093e698cfeb2ddf0b9
IwonaLalak <lalak.iwona@gmail.com>
2017-11-06 20:36:29 +0000

    add users form basics

--- app/frontend/practisebook/src/pages/Users/UserAddContainer.js
+++ app/frontend/practisebook/src/pages/Users/UserAddContainer.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
 
+import {Form, FormControl, FormGroup, ControlLabel, Col, Row} from 'react-bootstrap';
+import {ButtonSave} from "../../utilities/Buttons";
+
 import Header from '../../components/header/Header';
 
 export default class UserAddContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.handleAddNewUserClick = this.handleAddNewUserClick.bind(this);
+    }
+
+    handleAddNewUserClick(){
+
     }
 
     render(){
@@ -14,8 +22,80 @@ export default class UserAddContainer extends Component {
                 <div>
                     <Header url={["użytkownicy","dodaj"]}/>
                 </div>
-                <div>
-                    strona dodawania usera
+                <div id="ADD_USER">
+                    <div>
+                        <Form horizontal>
+                            <div>
+                                <Col sm={12} md={8} lg={7}>
+                                    <FormGroup>
+                                        <h5>Dane osobowe</h5>
+                                        <Row>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Imię</ControlLabel>
+                                                <FormControl type="text"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Nazwisko</ControlLabel>
+                                                <FormControl type="text"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Telefon</ControlLabel>
+                                                <FormControl type="number"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Email</ControlLabel>
+                                                <FormControl type="email"/>
+                                            </Col>
+                                        </Row>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <h5>Dane konta</h5>
+                                        <Row>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Login</ControlLabel>
+                                                <FormControl type="text"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Hasło</ControlLabel>
+                                                <FormControl type="password"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Grupa</ControlLabel>
+                                                <FormControl/>
+                                            </Col>
+                                        </Row>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <h5>Dane pracownika firmy</h5>
+                                        <Row>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Firma</ControlLabel>
+                                                <FormControl/>
+                                            </Col>
+                                        </Row>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <h5>Dane studenta</h5>
+                                        <Row>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Kierunek</ControlLabel>
+                                                <FormControl type="text"/>
+                                            </Col>
+                                            <Col md={6} lg={6}>
+                                                <ControlLabel>Semestr</ControlLabel>
+                                                <FormControl type="number"/>
+                                            </Col>
+                                        </Row>
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <div className="pull-right">
+                                            <ButtonSave onClick={this.handleAddNewUserClick}/>
+                                        </div>
+                                    </FormGroup>
+                                </Col>
+                            </div>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
--- app/frontend/practisebook/src/utilities/Buttons.js
+++ app/frontend/practisebook/src/utilities/Buttons.js
@@ -55,3 +55,30 @@ ButtonAdd.defaultProps = {
     btnSize: 'small',
     btnDisabled: false
 };
+
+export const ButtonSave = (props) => {
+    return (
+        <Button onClick={props.onClick}
+                bsStyle={props.btnColor}
+                bsSize={props.btnSize}
+                disabled={props.btnDisabled}
+        ><i className={props.iconType}></i> {props.btnText}</Button>
+    );
+};
+
+ButtonSave.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    btnText: PropTypes.string,
+    btnColor: PropTypes.string,
+    btnSize: PropTypes.string,
+    iconType: PropTypes.string,
+    btnDisabled: PropTypes.bool
+};
+
+ButtonSave.defaultProps = {
+    btnText: 'Zapisz',
+    iconType: 'fa fa-save',
+    btnColor: 'success',
+    btnSize: 'small',
+    btnDisabled: false
+};
