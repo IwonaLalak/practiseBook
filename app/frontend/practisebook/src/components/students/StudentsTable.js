3605be7da68caab28f6525591ef87902693bf50b
IwonaLalak <lalak.iwona@gmail.com>
2017-12-17 18:08:26 +0000

    students page

---
+++ app/frontend/practisebook/src/components/students/StudentsTable.js
@@ -0,0 +1,156 @@
+import React, {Component} from 'react';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {ButtonToolbar} from 'react-bootstrap';
+import tabgrid from '../../utilities/TabGrid';
+import {TableBtnSee} from '../../utilities/Buttons';
+import ReactTooltip from 'react-tooltip'
+import {Link} from "react-router-dom";
+
+
+export default class StudentsTable extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.renderActionButtons = this.renderActionButtons.bind(this);
+        this.onSeeClick = this.onSeeClick.bind(this);
+        this.renderLecturerOrLeader = this.renderLecturerOrLeader.bind(this);
+        this.renderCompany = this.renderCompany.bind(this);
+    }
+
+    onSeeClick(id) {
+        // this.props.handleSeeClick(id);
+    }
+
+    renderActionButtons(cell, row) {
+        return (
+            <ButtonToolbar>
+                <Link to={'student/' + row.user_id}>
+                    <TableBtnSee
+                        title={'Zobacz stronę studenta'}
+                        onClick={() => {
+                        }}
+                    />
+                </Link>
+            </ButtonToolbar>
+        )
+    }
+
+    renderStudent(cell, row) {
+        return (
+            <div>
+                <Link to={'student/' + row.user_id}>
+                    <p data-tip='' data-for={'student_data' + cell} data-place="bottom">{row.firstname} {row.lastname}</p>
+                </Link>
+                <ReactTooltip id={'student_data' + cell}>
+                    <p>{row.phone}</p>
+                    <p>{row.email}</p>
+                    <p>{row.study}, {row.semester}</p>
+                </ReactTooltip>
+            </div>
+        )
+    }
+
+    renderLecturerOrLeader(cell, row) {
+        let user = this.props.users.find(user => user.user_id == cell);
+        if (user) {
+            return (
+                <div>
+                    <p data-tip='' data-for={'user_data' + cell} data-place="bottom">{user.firstname} {user.lastname}</p>
+                    <ReactTooltip id={'user_data' + cell}>
+                        <p>{user.phone}</p>
+                        <p>{user.email}</p>
+                    </ReactTooltip>
+                </div>
+            )
+        }
+        else return cell
+    }
+
+    renderCompany(cell, row) {
+
+        let company = this.props.companies.find(company => company.company_id == cell);
+        if (company) {
+            return (
+                <div>
+                    <p data-tip='' data-for={'company_data' + cell} data-place="bottom">{company.name}</p>
+                    <ReactTooltip id={'company_data' + cell}>
+                        <p>{company.brand}</p>
+                        <p>{company.phone}</p>
+                        <p>{company.email}</p>
+                        <p>{company.city}, {company.street} {company.place}</p>
+                    </ReactTooltip>
+                </div>
+            )
+        }
+        else return cell
+    }
+
+    render() {
+
+        const options = {sizePerPageList: [10, 25, 50], sizePerPage: 25};
+        return (
+            <div>
+                <BootstrapTable data={this.props.students}
+                                hover
+                                pagination
+                                options={options}
+                                bordered={false}
+                >
+                    <TableHeaderColumn isKey dataField='user_id' hidden>ID</TableHeaderColumn>
+                    <TableHeaderColumn dataField='lastname' thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false} dataFormat={this.renderStudent}>Student</TableHeaderColumn>
+                    <TableHeaderColumn dataField='date_start' thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false}>Data początku praktyki</TableHeaderColumn>
+                    <TableHeaderColumn dataField='date_end' thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false}>Data końca praktyki</TableHeaderColumn>
+                    <TableHeaderColumn dataField='total_time' thStyle={tabgrid.tg2} tdStyle={tabgrid.tg2}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false}>Długość (h)</TableHeaderColumn>
+                    <TableHeaderColumn dataField='company_id' thStyle={tabgrid.tg2} tdStyle={tabgrid.tg2}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false} dataFormat={this.renderCompany}>Zakład pracy</TableHeaderColumn>
+                    <TableHeaderColumn dataField='leader_id' thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false}
+                                       hidden={(localStorage.getItem('current_usergroup') == 3)}
+                                       dataFormat={this.renderLecturerOrLeader}
+                    >Leader</TableHeaderColumn>
+                    <TableHeaderColumn dataField='lecturer_id' thStyle={tabgrid.tg3} tdStyle={tabgrid.tg3}
+                                       filter={(this.props.enableFilters) ? {
+                                           type: 'TextFilter',
+                                           delay: 500,
+                                           placeholder: 'Szukaj'
+                                       } : false}
+                                       hidden={(localStorage.getItem('current_usergroup') == 2)}
+                                       dataFormat={this.renderLecturerOrLeader}
+                    >Wykładowca</TableHeaderColumn>
+                    <TableHeaderColumn dataField='user_id' thStyle={tabgrid.tg1} tdStyle={tabgrid.tg1} dataFormat={this.renderActionButtons}
+                    > Akcje
+
+                    </TableHeaderColumn>
+                </BootstrapTable>
+            </div>
+        )
+    }
+}
--- app/frontend/practisebook/src/pages/Students/StudentsContainer.js
+++ app/frontend/practisebook/src/pages/Students/StudentsContainer.js
@@ -1,21 +1,88 @@
 import React, {Component} from 'react';
 
 import Header from '../../components/header/Header';
+import StudentsTable from "../../components/students/StudentsTable";
+import GeneralTop from "../../components/generaltop/GeneralTop";
+import StudentsService from "./StudentsService";
+import UsersService from "../Users/UsersService";
+import CompanyService from "../Companies/CompanyService";
 
 export default class StudentsContainer extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            students: [],
+            users:[],
+            companies:[],
+            filtersState: false,
+        };
+        this.getStudents = this.getStudents.bind(this);
+        this.getCompanies = this.getCompanies.bind(this);
+        this.getUsers = this.getUsers.bind(this);
+        this.handleClickEnableSearch = this.handleClickEnableSearch.bind(this);
     }
 
-    render(){
+    handleClickEnableSearch() {
+        this.setState({filtersState: !this.state.filtersState});
+    }
+
+    getUsers(){
+        UsersService.getAllUsers().then(function (response) {
+            this.setState({users:response.data})
+        }.bind(this))
+    }
+
+    getCompanies(){
+        CompanyService.getAllCompanies().then(function (response) {
+            this.setState({companies:response.data})
+        }.bind(this))
+    }
+
+    getStudents(){
+        if(localStorage.getItem('current_usergroup') == '2'){
+            // lecturers
+            StudentsService.getStudentsByLecturer(localStorage.getItem("current_userid")).then(function (response) {
+                console.log(response)
+                this.setState({students: response.data});
+            }.bind(this))
+        }
+        else{
+            // leaders
+            StudentsService.getStudentsByLeader(localStorage.getItem("current_userid")).then(function (response) {
+                console.log(response)
+                this.setState({students: response.data});
+            }.bind(this))
+        }
+    }
+
+    componentDidMount(){
+     this.getStudents();
+     this.getCompanies();
+     this.getUsers();
+    }
+
+    render() {
         return (
             <div>
                 <div>
-                    <Header url={[{url:'studenci',text:'studenci'},{url:'',text:'przegląd'}]}/>
+                    <Header url={[{url: 'studenci', text: 'studenci'}, {url: '', text: 'przegląd'}]}/>
                 </div>
-                <div>
-                    strona studentow
+                <div id="ALL_STUDENTS">
+                    <div>
+                        <GeneralTop
+                                    handleClickEnableSearch={this.handleClickEnableSearch}
+                                    disableForParticularGroup={true}
+                                    disableFor={localStorage.getItem("current_usergroup")}
+                        />
+                    </div>
+                    <div style={{clear: 'both'}}>
+                        <StudentsTable
+                            students={this.state.students}
+                            users={this.state.users}
+                            companies={this.state.companies}
+                            enableFilters={this.state.filtersState}
+                        />
+                    </div>
                 </div>
             </div>
         )
